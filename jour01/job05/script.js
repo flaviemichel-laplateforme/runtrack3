cebc5c77beae42b2bf09366b80e2719f633c4ca0
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-04 13:27:57 +0100

    JOB 05
    Fini
    
    Création de la fonction afficherjourssemaines(); avec une boucle forEach et une for

--- jour01/job05/script.js
+++ jour01/job05/script.js
@@ -1,41 +1,22 @@
 function afficherjourssemaines() {
 
-    const jourssemaines = [];
-    jourssemaines.push("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche");
-    console.log(jourssemaines); // 3
+    const jourssemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+    jourssemaines.forEach((item) => {
+        console.log(`${item}`);
+    });
+
 }
 
 afficherjourssemaines();
 
-
-// function bisextile(annee) {
-//     let result = (annee % 4 === 0 && annee % 100 != 0) || annee % 400 === 0
-//         ? `${annee} est une année bisextile`//si vrai
-//         : `${annee} n'est pas une année bisextile`;//Sinon
-//     console.log(result);
-// }
-
-// bisextile(2026);
+function afficherjourssemaines2() {
 
 
-// function dire(msg) {
-//   console.log(msg);
-// }
+    const jourssemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+    const iterateur = jourssemaines.keys();
+    for (const key of iterateur) {
+        console.log(`${jourssemaines[key]}`);
+    }
+}
 
-
-// if (age >= 18) {
-//   console.log("Majeur");
-// } else {
-//   console.log("Mineur");
-// }
-
-// for (let i = 0; i < 5; i++) {
-//   console.log(i);
-// }
-
-
-// function Hello(message) {
-//     console.log(message);
-// }
-
-// Hello("Bonjour Jean !");
+afficherjourssemaines2();

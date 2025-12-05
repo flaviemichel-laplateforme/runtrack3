51cfcb01fc97e6ceeadb8d8f5dae6760f0de82e9
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-05 16:53:44 +0100

    JOB05 EN COURS

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-   <textarea  id="keylogger"></textarea>
+<h1>Scroller vers le bas</h1>
+<footer>La plateforme_</footer>
     
     
 

d1d1f6974a8902f423531c1bd3aaf134b26cc0b8
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-05 15:43:00 +0100

    INIT JOB03

---
+++ jour02/job03/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <article id="texte">“L'important n'est pas la chute, mais l'atterrissage.”</article>
+    <button id="button" onclick="showhide()">Afficher</button> 
+    
+
+   
+
+ <script src="./script.js"></script>
+</body>
+
+</html>

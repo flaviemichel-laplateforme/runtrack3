b10e76f63bf1d81bfbf81da6018326b496939a77
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-05 15:05:53 +0100

    INIT JOB02

---
+++ jour02/job02/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <article id="citation">La vie a beaucoup plus d’imagination que nous.</article>
+    <button id="button" onclick="citation()">Afficher</button>
+
+   
+
+
+
+ <script src="./script.js"></script>
+</body>
+
+</html>

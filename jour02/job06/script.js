f6c855f09799ffdc1f9d3d10a554e8de8a283042
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-08 10:05:51 +0100

    FINI JOB06 konami activé

---
+++ jour02/job06/script.js
@@ -0,0 +1,40 @@
+// Le code Konami : ↑ ↑ ↓ ↓ ← → ← → B A
+const konamiCode = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+
+];
+console.log(konamiCode);
+let konamiIndex = 0; //Position actuelle
+
+document.addEventListener("keydown", function (event) {
+    const key = event.key.toLowerCase();
+    const expectedKey = konamiCode[konamiIndex].toLocaleLowerCase();
+
+    if (key === expectedKey) {
+        //SI bonne touche, on passe à la suivante
+        konamiIndex++;
+
+        if (konamiIndex === konamiCode.length) {
+            activerLaPlateforme();
+            konamiIndex = 0;
+        }
+
+    } else {
+        // Mauvaise touche, on recommence
+        konamiIndex = 0;
+    }
+});
+
+function activerLaPlateforme() {
+    document.body.classList.add("laplateforme");
+    console.log("Code Konami activé ! Bienvenue à la Plateforme_");
+}

86fea50d8d683990612d975e80c4cc59d1702fa1
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-10 10:23:10 +0100

    INIT JOB 03

---
+++ jour03/job03/script.js
@@ -0,0 +1,10 @@
+// Job 03
+
+// Prenez ce logo de La Plateforme_ et réalisez un jeu du taquin:
+// Le taquin est composé de 8 carreaux qui glissent dans une grille prévue pour 9.
+// Il consiste à remettre dans l'ordre les 8 carreaux à partir d'une configuration initiale.Le
+// plateau de jeu est initialisé de façon aléatoire.
+//     Lorsqu’un carreau est cliqué, il se déplace dans la case vide adjacente(si il y en a une).
+// Lorsque l’image est correctement recomposée, le message “Vous avez gagné” s’affiche
+// en vert et bloque la partie.
+// Un bouton “Recommencer” apparaît et permet de relancer une partie.

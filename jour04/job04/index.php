71cb476f24ec6ac1fc92f705b3c0493d19376bbf
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-11 09:17:46 +0100

    INIT JOB04

---
+++ jour04/job04/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+   
+   
+
+    <script src="./script.js"></script>
+</body>
+
+<!-- </html>Téléchargez le fichier suivant : pokemon.json
+Créez un formulaire permettant de trier ces données.
+Il doit contenir les champs suivants :
+● id (input type text),
+● nom (input type text),
+● type (liste déroulante <select>)
+● filtrer (input type button).
+Lorsque l’on clique sur “filtrer”, le script doit à l’aide de Fetch, récupérer le contenu du
+fichier et lister les éléments répondant aux critères sélectionnés en les affichant sur une
+page HTML.  -->

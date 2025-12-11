f9d4aed9270aa9b1a7f2a7e20fb40f3b8a70b6cb
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-11 09:27:24 +0100

    AJOUT de la structure de la page users.php et mise à jour des commentaires dans index.php

--- jour04/job04/index.php
+++ jour04/job04/index.php
@@ -8,19 +8,23 @@
 </head>
 
 <body>
-   
+   <!-- Job 04
+
+Créez une base de données “utilisateurs” contenant une table “utilisateurs” et ayant
+comme champs “id”, “nom”, “prenom” et “email”.
+Ajoutez des utilisateurs directement dans phpmyadmin.
+
+Créez une page users.php qui se connecte à la base de données, récupère l’ensemble
+des utilisateurs et affiche ces informations au format json.
+
+Dans votre page index.php, créez un tableau <table> permettant de contenir ces
+informations ainsi qu’un <button> “update”. Lorsque l’on clique sur ce bouton, le tableau
+doit se mettre à jour et contenir l’ensemble des informations des utilisateurs présents
+dans la base de données.
+Vous pouvez tester votre code en ajoutant/supprimant des utilisateurs à l’aide de
+phpmyadmin entre deux clics. -->
    
 
     <script src="./script.js"></script>
 </body>
 
-<!-- </html>Téléchargez le fichier suivant : pokemon.json
-Créez un formulaire permettant de trier ces données.
-Il doit contenir les champs suivants :
-● id (input type text),
-● nom (input type text),
-● type (liste déroulante <select>)
-● filtrer (input type button).
-Lorsque l’on clique sur “filtrer”, le script doit à l’aide de Fetch, récupérer le contenu du
-fichier et lister les éléments répondant aux critères sélectionnés en les affichant sur une
-page HTML.  -->

2a0133f6931b9120ba11080530fdfbcc31db8ac3
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-12-15 16:17:04 +0100

    Mise à jour de la structure HTML et ajout de nouveaux éléments de formulaire

--- jour06/job01/index.php
+++ jour06/job01/index.php
@@ -1,132 +1,213 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 
-     <!-- CSS Bootstrap -->
-    <link href="./assets/bootstrap-5.3.8/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <!-- CSS Bootstrap -->
+  <link href="./assets/bootstrap-5.3.8/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
+
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                </div>
-            </div>
-        </nav>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">LPTF</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Units</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Jobs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Skills</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-    </header>
+  </header>
 
-    <main class="container-fluid bg-secondary ">
-        
-       <h1 class="display-3 text-center">LaPlateforme_</h1>
+  <main class="container-fluid bg-secondary ">
 
-            <section class="row gap-1 m-3 ">
+    <h1 class="display-3 text-center">LaPlateforme_</h1>
 
-                <article class="row gap-3 .mx-auto display: block with: 100%">
-                    <div class="card" style="width: 15%;">
-                        <img src="./assets\img\papillon.jpg" class="card-img-top" alt="Papillon">
+    <section class="row gap-1 m-3 ">
 
-                        <div class="card-body">
-                            <h5 class="card-title">Un Papillon</h5>
-                            <p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes.<br>
-                            Ne pas ingérer.
-                             </p>
-                            <a href="#" class="btn btn-primary">Commander votre propre papillon</a>
-                        </div>
-                    </div>
+      <article class="row gap-3 .mx-auto display: block with: 100%">
+        <div class="card" style="width: 15%;">
+          <img src="./assets\img\papillon.jpg" class="card-img-top" alt="Papillon">
 
-                     <div class="card" style="width: 70%;">
-            
-                     <div class="card-body">
-                        <h5 class="card-title display-3 mt-4">Bonjour, monde!</h5>
-                         <p class="card-text lh-1.8 fs-4">Il existe plusieurs visions du terme:<br>
-                            le monde est la matière,l'espace et les phénomènes qui nous sont accécibles par les sens,l'expérience ou la raison.<br>
-                            Le sens le plus courant désigne notre planète. La terre,avec ses habitants, et son envoronnemnt plus ou moins naturel.</p>
-                    <hr>
-                        <p>Le sens étendu désigne l'univers dans son ensemble.</p>
-                      
-                       <a href="#" class="btn btn-danger">Rebooter le Monde</a>
-                        
-                    <div class="spinner-border ms-auto" aria-hidden="true"></div>
-                </div>
+          <div class="card-body">
+            <h5 class="card-title">Un Papillon</h5>
+            <p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes.<br>
+              Ne pas ingérer.
+            </p>
+            <a href="#" class="btn btn-primary">Commander votre propre papillon</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 70%;">
+
+          <div class="card-body">
+            <h5 class="card-title display-3 mt-4">Bonjour, monde!</h5>
+            <p class="card-text lh-1.8 fs-4">Il existe plusieurs visions du terme:<br>
+              le monde est la matière,l'espace et les phénomènes qui nous sont accécibles par les sens,l'expérience ou la raison.<br>
+              Le sens le plus courant désigne notre planète. La terre,avec ses habitants, et son envoronnemnt plus ou moins naturel.</p>
+            <hr>
+            <p>Le sens étendu désigne l'univers dans son ensemble.</p>
+
+            <a href="#" class="btn btn-danger">Rebooter le Monde</a>
+
+            <div class="spinner-border ms-auto" aria-hidden="true"></div>
+          </div>
 
 
 
-                        <nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-end">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
-                </div>
-            </div>
-            <div class="card" style="width: 10%;">
-            <ul class="list-group">
-  <li class="list-group-item active" aria-current="true">Limbes</li>
-  <li class="list-group-item">Luxure</li>
-  <li class="list-group-item">Gourmandise</li>
-  <li class="list-group-item">Avarice</li>
-  <li class="list-group-item">Colère</li>
-  <li class="list-group-item">Heresie</li>
-  <li class="list-group-item">Violence</li>
-  <li class="list-group-item">Ruse et Tromperie</li>
-  <li class="list-group-item">Trahison</li>
-  <li class="list-group-item">Internet </li>
-</ul>
-</div>
-            </div>
-        </article>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-end">
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        </div>
+        <div class="card" style="width: 10%;">
+          <ul class="list-group">
+            <li class="list-group-item active" aria-current="true">Limbes</li>
+            <li class="list-group-item">Luxure</li>
+            <li class="list-group-item">Gourmandise</li>
+            <li class="list-group-item">Avarice</li>
+            <li class="list-group-item">Colère</li>
+            <li class="list-group-item">Heresie</li>
+            <li class="list-group-item">Violence</li>
+            <li class="list-group-item">Ruse et Tromperie</li>
+            <li class="list-group-item">Trahison</li>
+            <li class="list-group-item">Internet </li>
+          </ul>
+        </div>
+        </div>
+      </article>
 
     </section>
-<div class="progress w-50 mx-auto" role="progressbar" aria-label="Warning example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-  <div class="progress-bar bg-warning" style="width: 90%"></div>
-</div>
-    </main>
 
-    <footer class="py-3 my-4"> 
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li> 
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li> 
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li> 
-            </ul> 
-            <p class="text-center text-body-secondary">© 2025 Company, Inc</p> 
-    </footer>
+    <div class="w-50 mx-auto">
+      <p class="text-end mb-2">Installation de AI 9000</p>
+      <div class="d-flex align-items-center gap-2">
+        <i class="bi bi-arrow-bar-left"></i>
 
-<!-- Link scrip bootstrap -->
-<script src="./assets/bootstrap-5.3.8/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        <!-- <div class="progress flex-grow-1" role="progressbar" aria-label="Warning example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar bg-warning" style="width: 90%"></div> -->
+
+        <div class="progress flex-grow-1" role="progressbar" aria-label="Animated striped example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: 90%"></div>
+
+        </div> <i class="bi bi-arrow-bar-right"></i>
+      </div>
+
+    </div>
+    </div>
+
+    <section class="row g-3 m-3 ">
+      <div class="col-md-6 ">
+        <div class="input-group mb-3 ">
+          <span class="input-group-text" id="basic-addon1">@</span>
+          <input type="text" class="form-control" placeholder="Login" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Mot de passe" aria-label="Recipient’s username" aria-describedby="basic-addon2">
+          <span class="input-group-text" id="basic-addon2">@example.com</span>
+        </div>
+        <div class="form-text" id="basic-addon4">URL des internets 2 et 2.1 Beta</div>
+        <div class="input-group mb-3">
+          <span class="input-group-text">DogeCoin</span>
+          <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+          <span class="input-group-text">.00</span>
+        </div>
+
+        <div class="mb-3">
+
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon3">https://133t.lptf/dkwb/berlusconimkt/</span>
+            <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4">
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="col-md-3">
+        <form class="row g-3 ">
+          <div class="col-12">
+            <label for="inputEmail4" class="form-label fw-medium">Email address</label>
+            <input type="email" class="form-control" id="inputEmail4">
+            <p class="fs-6 fw-light">We'll never share your email with anyone else.</p>
+          </div>
+          <div class="col-12">
+            <label for="inputPassword4" class="form-label fw-medium">Password</label>
+            <input type="password" class="form-control" id="inputPassword4">
+          </div>
+
+          <div class="col-12">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck">
+              <label class="form-check-label" for="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </section>
+
+
+
+  </main>
+
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2025 Company, Inc</p>
+  </footer>
+
+  <!-- Link scrip bootstrap -->
+  <script src="./assets/bootstrap-5.3.8/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
+
 </html>

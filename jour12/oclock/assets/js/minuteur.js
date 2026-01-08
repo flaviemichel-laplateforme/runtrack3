c5d919179280a630da5ce8421e724ff0e260752f
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-01-08 11:55:14 +0100

    feat: ajouter la fonctionnalité de minuteur avec interface utilisateur essai

---
+++ jour12/oclock/assets/js/minuteur.js
@@ -0,0 +1,18 @@
+const minuteurInput = document.getElementById('minuteurInput');
+
+function formatNumber(num) {
+    return num < 10 ? "0" + num : num;
+
+}
+console.log(formatNumber(5));
+
+
+function minuteur() {
+    const heures = new hours;
+
+}
+
+console.log(heures);
+
+const minuteurDepart = minuteurInput + 5;
+console.log(minuteurDepart);

220b86a11cbd114109e7ad5ba4b5905787e7fa93
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-01-08 11:10:35 +0100

    feat: ajouter la fonctionnalité d'horloge avec affichage de l'heure actuelle

---
+++ jour12/oclock/assets/js/horloge.js
@@ -0,0 +1,13 @@
+const heureInput = document.getElementById('timeInput')
+
+const heureActuelle = new Date();
+console.log(heureActuelle);
+
+const date = new Date();
+const heures = date.getHours();
+const minutes = date.getMinutes;
+const secondes = date.getSeconds();
+const millisecondes = date.getMilliseconds();
+
+
+

0c3b739080f266e29db117606b3c4154ae032bf9
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-01-08 11:36:49 +0100

    feat: améliorer la fonction d'horloge pour afficher l'heure actuelle en temps réel

--- jour12/oclock/assets/js/horloge.js
+++ jour12/oclock/assets/js/horloge.js
@@ -1,13 +1,28 @@
 const heureInput = document.getElementById('timeInput')
 
-const heureActuelle = new Date();
-console.log(heureActuelle);
+function formatNumber(num) {
+    return num < 10 ? "0" + num : num;
 
-const date = new Date();
-const heures = date.getHours();
-const minutes = date.getMinutes;
-const secondes = date.getSeconds();
-const millisecondes = date.getMilliseconds();
+}
+
+console.log(formatNumber(5));
+
+function updateTime() {
+    const date = new Date();
+    const heures = date.getHours();
+    const minutes = date.getMinutes();
+    const secondes = date.getSeconds();
+    const millisecondes = date.getMilliseconds();
+
+    const heureFormatee = `${formatNumber(heures)}:${formatNumber(minutes)}:${formatNumber(secondes)}`;
+    heureInput.value = heureFormatee;
+    console.log(heureInput);
+}
+
+
+updateTime();
+
+setInterval(updateTime, 1000);
 
 
 

31ac262abac657145aeaefc991ea5257e2b51927
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-01-09 11:24:22 +0100

    feat: refactor l'affichage de l'heure et ajouter le style CSS pour une meilleure présentation

--- jour12/oclock/assets/js/horloge.js
+++ jour12/oclock/assets/js/horloge.js
@@ -1,27 +1,20 @@
-const heureInput = document.getElementById('timeInput')
+const currentTimeDisplay = document.getElementById('currentTime');
 
 function formatNumber(num) {
     return num < 10 ? "0" + num : num;
-
 }
-
-console.log(formatNumber(5));
 
 function updateTime() {
     const date = new Date();
     const heures = date.getHours();
     const minutes = date.getMinutes();
     const secondes = date.getSeconds();
-    const millisecondes = date.getMilliseconds();
 
     const heureFormatee = `${formatNumber(heures)}:${formatNumber(minutes)}:${formatNumber(secondes)}`;
-    heureInput.value = heureFormatee;
-    console.log(heureInput);
+    currentTimeDisplay.textContent = heureFormatee;
 }
 
-
 updateTime();
-
 setInterval(updateTime, 1000);
 
 

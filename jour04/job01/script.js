d4ecd5b6878934ddabcf2b8831b27df405bf0246
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-01-09 14:55:54 +0100

    preparation deploiement plesk oclock

--- jour04/job01/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("button").addEventListener("click", async function () {
-    const reponse = await fetch("./expression.txt");
-    const citation = await reponse.text();
-    document.getElementById("citation").textContent = citation;
-    console.log("citation");
-});
